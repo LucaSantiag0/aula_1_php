cd500e5a0fcafb72616f69f642cf894c8b092fd5
Santiago <lucasoliveirasantiago3@gmail.com>
2024-08-30 13:12:09 -0300

    adicionado put e delete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -81,13 +81,33 @@ class UserController extends Controller
     public function update(Request $request, string $id)
     {
         //
+        $data = $request->all();
+        try {
+            $user = User::findOrFail($id);
+            $user->update($data);
+
+        } catch (\Exception $e) {
+            return [
+                'status' => 400,
+                'menssagem' => 'Erro ao atualizar usuário!!',
+                'error' => $e->getMessage()
+            ];
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
+        try {
+            $user = User::findOrFail($id);
+            $user->delete();
+
+        } catch (\Exception $e) {
+            return [
+                'status' => 400,
+                'menssagem' => 'Erro ao deletar usuário!!',
+                'error' => $e->getMessage()
+            ];
+        }
     }
 }
